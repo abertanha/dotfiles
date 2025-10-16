60d8224dc7101540dc00cc4183cdb0692edd4da6
Adriano Bertanha <110491311+abertanha@users.noreply.github.com>
2025-10-16 09:45:22 -0300

    feat: Implementando a instalação do mongosh

--- setup-1.sh
+++ setup-1.sh
@@ -48,12 +48,16 @@ code --install-extension PKief.material-icon-theme     # Material Icons
 code --install-extension zhuangtongfa.Material-theme   # One Dark Pro
 
 ########################################
-# Instalando MongoDB Compass (GUI apenas)
+# Instalando MongoDB Compass e MongoSH
 wget https://downloads.mongodb.com/compass/mongodb-compass_1.42.0_amd64.deb -O /tmp/mongodb-compass.deb
 sudo dpkg -i /tmp/mongodb-compass.deb
 sudo apt-get install -f
 rm /tmp/mongodb-compass.deb
 
+curl -fsSL https://www.mongodb.org/static/pgp/server-6.0.asc | sudo gpg --dearmor -o /etc/apt/trusted.gpg.d/mongodb-6.gpg
+echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu jammy/mongodb-org/6.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-6.0.list
+sudo apt install -y mongodb-mongosh
+mongosh --version
 ########################################
 # Installing DBeaver CE and Postman
 sudo snap install dbeaver-ce
